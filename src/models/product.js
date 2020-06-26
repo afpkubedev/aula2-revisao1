d173e349309da97d118f3fe03ac40ef7ce01dc49
Alisson Fernandes de Paula <pfalisson@gmail.com>
2020-06-26 10:44:27 -0300

    API products jornadakubernetes (name, price, category, description)

---
+++ src/models/product.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ProductSchema = new Schema({
+    name: {type: String, required: true, max: 100},
+    category: {type: String, required: true, max: 50},
+    price: {type: Number, required: true},
+    description: {type: String, required: true, max: 50}
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
